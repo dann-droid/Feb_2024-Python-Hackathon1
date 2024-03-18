9c70e81e0e568aa8b08cc2d24458cd02526590c5
Danson Ngenya <ngenyadann254@gmail.com>
2024-03-19 01:23:26 +0300

    PYTHON HACKATHON 1
    Fibonacci sequence program and an if-else conditional

---
+++ PyHackathon/condition.py
@@ -0,0 +1,11 @@
+
+age = int(input("How Old Are You? "))
+
+#check age
+if age >= 18:
+    print("You are Eligible to Vote!")
+else:
+    print("You are Not Eligible to Vote!")
+
+
+
---
+++ PyHackathon/fibonacci.py
@@ -0,0 +1,12 @@
+n = int(input("Enter the value of n:> "))
+
+def fibonacci(n):
+    fib_seq = [0, 1]
+    while len(fib_seq) < n:
+        fib_seq.append(fib_seq[-1] + fib_seq[-2])
+    return fib_seq[:n]
+
+    
+print(f"The first {n} terms are: ")
+print(fibonacci(n))
+
